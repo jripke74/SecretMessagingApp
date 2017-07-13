f82fd160ebba84847ae14351c3f0be6dc9134b08
Jeff Ripke <jripke74@gmail.com>
2017-07-13 12:50:03 -0700

    initial commit

---
+++ MessagesExtension/MessageCompactViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MessageCompactViewController.swift
+//  SecretMessage
+//
+//  Created by Jeff Ripke on 7/13/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class MessageCompactViewController: UIViewController {
+
+    
+
+}
